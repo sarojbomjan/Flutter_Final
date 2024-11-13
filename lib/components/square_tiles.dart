99b30c3ab355131e42302253807e51fb70b54e99
.android <sarojbomjan7@gmail.com>
2024-11-13 21:02:40 +0545

    Sign In Option Added

--- lib/components/square_tiles.dart
+++ lib/components/square_tiles.dart
@@ -7,7 +7,16 @@ class SquareTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Image.asset(imagePath),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey[200],
+        ),
+      child: Image.asset(
+        imagePath,
+        height: 40,
+        ),
     );
   }
 }
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -16,117 +16,138 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      resizeToAvoidBottomInset: true,   // Automatically resizes the body when the keyboard shows up
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              //logo
-              Icon(
-                Icons.lock,
-                size: 100,
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                "Welcome to BookStore",
-                style: TextStyle(color: Colors.grey[700], fontSize: 30),
-              ),
-
-              SizedBox(
-                height: 35,
-              ),
-
-              //username textfield
-              MyTextfield(
-                controller: usernameController,
-                hintText: 'Username',
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              //password textfield
-              MyTextfield(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true,
-              ),
-
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
               const SizedBox(
-                height: 30,
-              ),
+                  height: 50,
+                ),
+                //logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+               const SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Welcome to BookStore",
+                  style: TextStyle(color: Colors.grey[700], fontSize: 30),
+                ),
+          
+                const SizedBox(
+                  height: 35,
+                ),
+          
+                //username textfield
+                MyTextfield(
+                  controller: usernameController,
+                  hintText: 'Username', 
+                  obscureText: false,
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                //password textfield
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+          
+                const SizedBox(
+                  height: 30,
+                ),
+          
+                // forgot password  // row is used to align text at end
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password",
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
+          
+                const SizedBox(
+                  height: 30,
+                ),
+          
+                //sign in button
+                MyButton(
+                  onTap: signUserin,
+                ),
+          
+                const SizedBox(
+                  height: 50,
+                ),
+          
+                // or continue with
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          "Or continue with",
+                          style: TextStyle(color: Colors.grey[700], fontSize: 18),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
 
-              // forgot password  // row is used to align text at end
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+              const SizedBox(height: 50,),
+                // sign in options icons
+               const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Forgot Password",
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
+                    // google button
+                    SquareTile(imagePath: 'lib/images/google.png'),
+
+                    SizedBox(width: 20,),
+                    //twitter button
+                    SquareTile(imagePath: 'lib/images/twitterr.png'),
+
+                     SizedBox(width: 20,),
+
+                    SquareTile(imagePath: 'lib/images/facebook.jpg')
                   ],
                 ),
-              ),
 
-              const SizedBox(
-                height: 30,
-              ),
+                const SizedBox(height: 50,),
 
-              //sign in button
-              MyButton(
-                onTap: signUserin,
-              ),
-
-              const SizedBox(
-                height: 35,
-              ),
-
-              // or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        "Or continue with",
-                        style: TextStyle(color: Colors.grey[700], fontSize: 18),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
+                    Text("Don't have an account?", style: TextStyle(color: Colors.grey[700]),),
+                    const SizedBox(width: 4,),
+                    const Text("Register now", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),)
+
                   ],
-                ),
-              ),
-
-              // sign in options icons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // google button
-                  SquareTile(imagePath: 'lib/images/google.png'),
-
-                  //twitter button
-                  SquareTile(imagePath: 'lib/images/twiterr.png')
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
